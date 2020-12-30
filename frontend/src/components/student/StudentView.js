97c18599912796ccc036c7bffd245338b162f449
GrantPerkins <gcperk20@gmail.com>
2020-12-30 17:51:15 -0500

    Student view request hours

--- frontend/src/components/student/StudentView.js
+++ frontend/src/components/student/StudentView.js
@@ -1,35 +1,53 @@
 import React from "react";
 import Background from "../background/Background";
-import {Button, Card, CardContent, Grid} from "@material-ui/core";
-import FRCTable from "../utility/Table";
+import {
+    Button,
+    Card,
+    CardContent,
+    FormControl,
+    Grid,
+    InputLabel,
+    MenuItem,
+    Select,
+    TextField,
+    Typography
+} from "@material-ui/core";
+import FRCTable from "../utility/FRCTable";
+import HoursTable from "../utility/HoursTable";
 
 const styles = {
     tables: {
         padding: "2%"
-    }
+    },
+    formControl: {
+        width: "100%"
+    },
 }
 
 class StudentView extends React.Component {
-    tables = [<FRCTable title={"Your events"} secondaryTitle={"Date"} rows={[]}/>,
-        <FRCTable title={"Upcoming events"} secondaryTitle={"Date"} rows={[]}/>,
-        <FRCTable title={"Support hours"} secondaryTitle={"Week"} rows={[]}/>,
-        <FRCTable title={"Operations hours"} secondaryTitle={"Week"} rows={[]}/>,
+    tables = [
+        <FRCTable title={"Upcoming Events"} secondaryTitle={"Date"} rows={[]}/>,
+        <HoursTable title={"Operations Hours"} rows={[]}/>,
+        <FRCTable title={"Your Events"} secondaryTitle={"Date"} rows={[]}/>,
+        <HoursTable title={"Support Hours"} rows={[]}/>,
         <FRCTable title={"Votes"} secondaryTitle={"Date"} rows={[]}/>
     ];
+
     generateToolbarContent() {
         return (
+
             <Grid container spacing={4}>
                 <Grid item>
-                    <Button size={"large"} variant={"contained"}> Slack </Button>
+                    <Button variant={"contained"}> Slack </Button>
                 </Grid>
                 <Grid item>
-                    <Button size={"large"} variant={"contained"}> Records </Button>
+                    <Button variant={"contained"}> Hours </Button>
                 </Grid>
                 <Grid item>
-                    <Button size={"large"} variant={"contained"}> Events </Button>
+                    <Button variant={"contained"}> Events </Button>
                 </Grid>
                 <Grid item>
-                    <Button size={"large"} variant={"contained"}> Voting </Button>
+                    <Button variant={"contained"}> Voting </Button>
                 </Grid>
             </Grid>
         );
@@ -37,17 +55,72 @@ class StudentView extends React.Component {
 
     generateContent() {
         return (
-            <Grid container spacing={3} style={styles.tables}>
-                {this.tables.map((row) => {
-                    return (
-                        <Grid item xs={6}>
-                            <Card>
-                                <CardContent>{row}</CardContent>
-                            </Card>
-
+            <Grid container>
+                <Grid item xs={9}>
+                    <Grid container spacing={3} style={styles.tables}>
+                        <Grid item xs={12}>
+                            <Typography variant={"h3"}> Your data </Typography>
                         </Grid>
-                    )
-                })}
+                        {this.tables.map((row) => {
+                            return (
+                                <Grid item xs={6}>
+                                    <Card>
+                                        <CardContent>{row}</CardContent>
+                                    </Card>
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
+                </Grid>
+                <Grid item xs={3}>
+                    <Grid container>
+                        <Card>
+                            <CardContent>
+
+                                <Typography variant={"h5"}>
+                                    Request approval for hours worked
+                                </Typography>
+                                <form className="CreateChoiceForm" onSubmit={(event) => {
+                                    event.preventDefault()
+                                }}>
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={12}>
+                                            <FormControl style={styles.formControl}>
+                                                <InputLabel id={"studentApprovalHourLabel"}> Type of hours </InputLabel>
+                                                <Select required labelId={"studentApprovalHourLabel"}
+                                                        id={"studentApprovalHourType"}>
+                                                    <MenuItem value={"Support"}>Support</MenuItem>
+                                                    <MenuItem value={"Operations"}>Operations</MenuItem>
+                                                </Select>
+                                            </FormControl>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <FormControl style={styles.formControl}>
+                                                <TextField required type={"number"} id={"hoursWorked"} placeholder={0}/>
+                                            </FormControl>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <FormControl style={styles.formControl}>
+                                                <InputLabel id={"studentApprovalWeekLabel"}> Week earned </InputLabel>
+                                                <Select required labelId={"studentApprovalWeekLabel"}
+                                                        id={"studentApprovalWeek"}>
+                                                    {[0, 1, 2, 3, 4, 5, 6].map((value) => {
+                                                        return (
+                                                            <MenuItem key={value} value={value}>Week {value}</MenuItem>
+                                                        )
+                                                    })}
+                                                </Select>
+                                            </FormControl>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Button variant={"contained"} color={"primary"}>Submit</Button>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
             </Grid>
         );
     }
