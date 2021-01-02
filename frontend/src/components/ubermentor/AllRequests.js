4f362c5dce7fd41fff39cd4638582230ed30379a
GrantPerkins <gcperk20@gmail.com>
2021-01-02 18:34:32 -0500

    Sort by heading

--- frontend/src/components/ubermentor/AllRequests.js
+++ frontend/src/components/ubermentor/AllRequests.js
@@ -1,6 +1,6 @@
 import React from "react";
 import LambdaAPI from "../utility/LambdaAPI";
-import {Box, Grid, Table, TableBody, TableCell, TableHead, TableRow} from "@material-ui/core";
+import {Box, Grid, Table, TableBody, TableCell, TableHead, TableRow, TableSortLabel} from "@material-ui/core";
 import {withAuth0} from "@auth0/auth0-react";
 
 const styles = {
@@ -13,7 +13,15 @@ class AllRequests extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            requests: []
+            requests: [],
+            headerCells: [
+                {name: "Type", width: "10%", id: "request_type"},
+                {name: "Name", width: "20%", id: "member_name"},
+                {name: "Date", width: "10%", id: "date"},
+                {name: "Data", width: "20%", id: "data"}
+            ],
+            order: "asc",
+            orderBy: "date"
         }
     }
 
@@ -48,28 +56,76 @@ class AllRequests extends React.Component {
         );
     }
 
+    descendingComparator(a, b, orderBy) {
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    }
+
+    stableSort(array, comparator) {
+        const stabilizedThis = array.map((el, index) => [el, index]);
+        stabilizedThis.sort((a, b) => {
+            const order = comparator(a[0], b[0]);
+            if (order !== 0) return order;
+            return a[1] - b[1];
+        });
+        return stabilizedThis.map((el) => el[0]);
+    }
+
+    getComparator(order, orderBy) {
+        return order === 'desc'
+            ? (a, b) => this.descendingComparator(a, b, orderBy)
+            : (a, b) => -this.descendingComparator(a, b, orderBy);
+    }
+
+
+    createSortHandler = (property) => (event) => {
+        console.log("handling");
+        const {order, orderBy} = this.state;
+        const isAsc = orderBy === property && order === 'asc';
+        this.setState({order: isAsc ? 'desc' : 'asc', orderBy: property});
+    };
+
     render() {
+        const {order, orderBy} = this.state;
         return (
             <Table>
                 <TableHead>
                     <TableRow>
-                        <TableCell style={styles.heading} width={"10%"}> Type </TableCell>
-                        <TableCell style={styles.heading} width={"20%"}> Name </TableCell>
-                        <TableCell style={styles.heading} width={"10%"}> Date </TableCell>
-                        <TableCell style={styles.heading} width={"60%"}> Data </TableCell>
+                        {this.state.headerCells.map(value => {
+                            const {width, name, id} = value;
+                            const {order, orderBy} = this.state;
+                            return (
+                                <TableCell width={width} style={styles.heading} sortDirection={orderBy === id ? order : false}>
+                                    <TableSortLabel
+                                        active={orderBy === id}
+                                        direction={orderBy === id ? order : 'asc'}
+                                        onClick={this.createSortHandler(id)}
+                                    >
+                                        {name}
+                                    </TableSortLabel>
+                                </TableCell>
+                            );
+                        })}
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {this.state.requests.map((value) => {
-                        return (
-                            <TableRow key={value["uuid"]}>
-                                <TableCell>{value["request_type"]}</TableCell>
-                                <TableCell>{value["member_name"]}</TableCell>
-                                <TableCell>{value["date"]}</TableCell>
-                                <TableCell>{this.generateData(value["data"], value["request_type"])}</TableCell>
-                            </TableRow>
-                        );
-                    })}
+                    {
+                        this.stableSort(this.state.requests, this.getComparator(order, orderBy))
+                            .map((value) => {
+                                return (
+                                    <TableRow key={value["uuid"]}>
+                                        <TableCell>{value["request_type"]}</TableCell>
+                                        <TableCell>{value["member_name"]}</TableCell>
+                                        <TableCell>{value["date"]}</TableCell>
+                                        <TableCell>{this.generateData(value["data"], value["request_type"])}</TableCell>
+                                    </TableRow>
+                                );
+                            })}
                 </TableBody>
             </Table>
         );
